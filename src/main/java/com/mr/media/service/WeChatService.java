a025fa9c2158a2ad4742db8b8ef1e1f62c7ce5f0
Qin, Bruno <bruno.qin@sap.com>
2017-01-10 17:16:06 +0800

    :wrench: refactor

--- src/main/java/com/mr/media/service/WeChatService.java
+++ src/main/java/com/mr/media/service/WeChatService.java
@@ -1,30 +1,16 @@
 package com.mr.media.service;
 
-import com.mr.media.Application;
 import com.mr.media.util.WeChatHelper;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.*;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by i321273 on 1/9/17.
  */
 
 @Service
-@Component
-@EnableScheduling
 public class WeChatService {
 
     final Logger logger = LoggerFactory.getLogger(getClass());
