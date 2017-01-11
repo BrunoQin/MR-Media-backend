a95b79c4648736c62cb708ad29a4043b19abae3d
Bruno Qin <brunoqin@163.com>
2017-01-12 00:29:04 +0800

    :bug: fix bug

--- src/test/java/com/mr/media/test/controller/UserControllerTest.java
+++ src/test/java/com/mr/media/test/controller/UserControllerTest.java
@@ -3,9 +3,10 @@ package com.mr.media.test.controller;
 import com.mr.media.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.junit.Assert.assertTrue;
 
@@ -15,7 +16,8 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-@WebIntegrationTest
+@WebAppConfiguration
+@IntegrationTest
 public class UserControllerTest {
 
 //    @Autowired
