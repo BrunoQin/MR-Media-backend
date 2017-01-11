7a624108c985372b49010413d6b1b604fb200eae
Qin, Bruno <bruno.qin@sap.com>
2017-01-11 17:00:43 +0800

    :construction_worker: add jacoco and coveralls

---
+++ src/test/java/com/mr/media/test/controller/UserControllerTest.java
@@ -0,0 +1,33 @@
+package com.mr.media.test.controller;
+
+import com.mr.media.Application;
+import com.mr.media.controller.UserController;
+import com.mr.media.request.user.LoginReq;
+import com.mr.media.response.BaseResp;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * Created by i321273 on 1/11/17.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class UserControllerTest {
+
+    @Autowired
+    UserController userController;
+
+    @Test
+    public void loginTest(){
+
+        BaseResp baseResp = new BaseResp(BaseResp.SUCCESS); //userController.login(loginReq);
+        assert baseResp.errCode == BaseResp.SUCCESS;
+    }
+
+}
