ba71a9cebfcce7d1afb5381d021d8ad12c5e47e9
谭靖儒 <tanjingru@tz.local>
2017-04-21 22:27:56 +0800

    ..

--- src/main/java/com/mr/media/service/authority/AdminService.java
+++ src/main/java/com/mr/media/service/authority/AdminService.java
@@ -118,6 +118,7 @@ public class AdminService {
             authority.setAuthority(auth);
             authority.save();
         }
+        
         Ebean.commitTransaction();
         Ebean.endTransaction();
         return new BaseResp(BaseResp.SUCCESS);
